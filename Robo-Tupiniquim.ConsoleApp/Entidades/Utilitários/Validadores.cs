e2168b3290b429248852919087a2e462c4d403e7
Agatha Sates <satesagatha@gmail.com>
2025-03-28 00:06:30 -0300

    feat: Adicionado classe validadores

---
+++ Robo-Tupiniquim.ConsoleApp/Entidades/Utilitários/Validadores.cs
@@ -0,0 +1,15 @@
+﻿namespace Robo_Tupiniquim.ConsoleApp.Entidades.Utilitários;
+
+class Validadores
+{
+    public static string opcaoContinuar;
+    public static void SimouNaoValido()
+    {
+        opcaoContinuar = Console.ReadLine()!.ToUpper();
+        while (string.IsNullOrWhiteSpace(opcaoContinuar) || opcaoContinuar.ToUpper() != "S" && opcaoContinuar.ToUpper() != "N")
+        {
+            Console.Write("(X) Opção inválida! Digite novamente S ou N: ");
+            opcaoContinuar = Console.ReadLine()!;
+        }
+    }
+}
