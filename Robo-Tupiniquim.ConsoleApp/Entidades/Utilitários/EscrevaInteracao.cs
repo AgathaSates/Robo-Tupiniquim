969cd091f63a1563ebdebfe0a44a0ea56d7ff7f2
Agatha Sates <satesagatha@gmail.com>
2025-03-28 00:07:35 -0300

    feat: Adicionado classe EscrevaInteração.

---
+++ Robo-Tupiniquim.ConsoleApp/Entidades/Utilitários/EscrevaInteracao.cs
@@ -0,0 +1,18 @@
+﻿namespace Robo_Tupiniquim.ConsoleApp.Entidades.Utilitários;
+
+class EscrevaInteracao
+{
+    public static bool Explorarnovamente()
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Deseja explorar uma nova área? (S/N): ");
+
+        Validadores.SimouNaoValido();
+
+        if (Validadores.opcaoContinuar == "N")
+            return false;
+
+        else
+            return true;
+    }
+}
