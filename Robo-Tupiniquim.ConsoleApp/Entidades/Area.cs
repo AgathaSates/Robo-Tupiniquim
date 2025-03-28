77c888aafc4e77325b3948ae181f53160432c4e0
Agatha Sates <satesagatha@gmail.com>
2025-03-28 05:20:22 -0300

    style: Reformulado interação com usuário.

--- Robo-Tupiniquim.ConsoleApp/Entidades/Area.cs
+++ Robo-Tupiniquim.ConsoleApp/Entidades/Area.cs
@@ -4,11 +4,17 @@ class Area
 {
     public static int LimiteX;
     public static int LimiteY;
-    public static void Area_()
+    public static void PedirArea()
     {
-        Console.Write("Digite o tamanho do mapa (x y): ");
+        Console.Write("-> Digite o tamanho da área (largura X altura, ex: 5 5): ");
         string[] limites = Console.ReadLine().Split(' ');
         LimiteX = int.Parse(limites[0]);
         LimiteY = int.Parse(limites[1]);
+
+        Console.WriteLine();
+        Console.WriteLine($"> Área de exploração definida: {LimiteX}x{LimiteY} km");
+        Console.WriteLine("----------------------------------------------------------");
+        Console.Write("Pressione qualquer tecla para continuar a missão:");
+        Console.ReadKey();
     } 
 }
